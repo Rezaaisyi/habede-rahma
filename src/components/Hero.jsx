e3e33ffdcb9a8ed75b3fcfeb39ece240cedce9dd
Rezaaisyi <reza.aisyi@gmail.com>
2023-01-26 17:31:50 +0700

    project baru untuk birthday

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} 
       flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-row items-center py-[6px] px-4 bg-pastelgreen rounded-[10px] mb-2 drop-shadow-[0_10px_10px_rgba(0,0,0,0.25)]"> 
+        <div className="flex flex-row items-center py-[6px] px-4 bg-pastelgreen rounded-[10px] mb-2 solid-shadow"> 
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <div className="flex flex-col">
                       <p className={`${styles.paragraph} ml-2 text-black`}>
